0f4ade8eacb0e95c06e0da98cb17e8cf9d6d3ef0
Михаил Кашинин <flyaway400@gmail.com>
2019-04-21 19:18:13 +0800

    divide carriges

--- carriage.rb
+++ carriage.rb
@@ -1,35 +1,11 @@
 require "./route.rb"
 class Carriage # класс вагон
 attr_accessor :type_carriage, :places
+  
   def carriage_display_info
     puts "carriage type = #{@type_carriage}" # тип вагончика
     puts "carriage places = #{@places}" # места (для пассажирского - одно значение, для грузового - другое значение)
   end
 end
 
-class PassengerCarriage < Carriage # пассажирский вагон - потомок класса вагон
-  def initialize
-    @type_carriage = 'pass'
-    @places = 46
-   end
-end
 
-class CargoCarriage < Carriage # грузовой вагон - потомок класса вагон
-  def initialize
-    @type_carriage = 'cargo'
-    @places = 30 
-   end
-end
-
-
-#тест
-=begin
-cargo1 = CargoCarriage.new
-cargo1.carriage_display_info
-cargo1.places
-
-
-pass1 = PassengerCarriage.new
-pass1.carriage_display_info
-pass1.places
-=end
