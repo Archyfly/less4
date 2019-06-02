9b9009e1cd7de2ea329ad32a4f4c95eed6ed5c76
Михаил Кашинин <flyaway400@gmail.com>
2019-06-03 00:41:47 +0800

    meta2

--- accessors.rb
+++ accessors.rb
@@ -15,12 +15,22 @@ module Accessors
         instance_variable_get("@#{var_name_history}") << value
      end
 
-     define_method ("#{var_name_history}") {instance_variable_get("@#{var_name_history}")}
+     define_method("#{var_name_history}") {instance_variable_get("@#{var_name_history}")}
   end
 
-  def strong_attr_accessor 
-  
+  def strong_attr_accessor(attr_name, attr_class)
+    define_method(attr_name) {instance_variable_get(attr_name)}
+    
+    define_method("#{attr_name}=") do |value|
+        if value.is_a? attr_class
+          instance_variable_set("@#{attr_name}", value) 
+      else 
+        raise ArgumentError.new("Invalid type")
+      end
+    end   
   end
+
+
 end
 
 
@@ -30,6 +40,6 @@ class Test
   extend Accessors
 
   attr_accessor_with_history :c, :d
-  
+  self.strong_attr_accessor(:d, Integer)
   
 end
