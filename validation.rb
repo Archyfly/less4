cc1a759a4067a1e3dc9132588a7e322f23535dee
Михаил Кашинин <flyaway400@gmail.com>
2019-06-04 00:35:27 +0800

    Validate!

--- validation.rb
+++ validation.rb
@@ -2,11 +2,17 @@ module Validation
   NUMBER_FORMAT = /^\w{3}\-?\w{2}$/i.freeze
   TYPE_TRAIN_FORMAT = /(?i)(\W|^)(pass|cargo)(\W|$)/.freeze
 
-  def validate(attr, *validate_types)
-    case validate_type
-      when "presence"
-      when "format" 
-      when "type, String"
+  def self.included(base)
+    base.extend validate
+    base.send :include, validate!, valid?
+  
+  def validate(attr, validate_type)
+    choice = validate_type 
+    case choice
+      when 'presence' then puts "its presence"
+      when 'format' then puts "its NUMBER_FORMAT"
+      when 'type' then puts "its type, string"
+    end      
   end
 
   def valid_number(number)
@@ -22,4 +28,27 @@ module Validation
   def valid_station(station_name)
     raise "Station name can't be nil" if station_name.size < 3
   end
+
+  def valid?
+    validate!
+  rescue StandardError
+    false
+  end
+
+  protected
+
+  def validate!
+    raise "Number can't be nil" if @number.nil?
+    raise 'Number should be at least 6 symbols' if @number.to_s.size < 5
+    raise 'Number has invalid format' if @number !~ NUMBER_FORMAT
+    true
+  end
+
+
 end
+class Test
+  extend Validation
+
+attr_accessor :per1, :per2
+
+  end
