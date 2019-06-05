6d8ebda13bc23b465bcb9878f6a52618d49f1d62
Михаил Кашинин <flyaway400@gmail.com>
2019-06-06 00:08:13 +0800

    validate 5

--- validation.rb
+++ validation.rb
@@ -1,6 +1,6 @@
 module Validation
-  NUMBER_FORMAT = /^\w{3}\-?\w{2}$/i.freeze
-  TYPE_TRAIN_FORMAT = /(?i)(\W|^)(pass|cargo)(\W|$)/.freeze
+  #NUMBER_FORMAT = /^\w{3}\-?\w{2}$/i.freeze
+  #TYPE_TRAIN_FORMAT = /(?i)(\W|^)(pass|cargo)(\W|$)/.freeze
 
   def self.included(base)
     base.extend ClassValids
@@ -8,18 +8,25 @@ module Validation
   end
 
   module ClassValids 
-  attr_reader validate_type
+  attr_reader :validations
     
-    def validate(attrib, validate_type)
-    choice = validate_type 
-    case choice
-      when 'presence' then valid_presence(attrib)
-      when 'format' then valid_format(attrib, valid_format)
-      when 'type' then valid_type(attrib, valid_type)
-      end      
+    def validate(attrib, validate_type, *args) # Этот метод принимает в качестве параметров имя проверяемого атрибута, а также тип валидации
+      @validations ||= {} # задаем хэш :переменная => аттрибуты 
+      @validations[attrib] ||= [] 
+      @validations[attrib] << { validate_type: validate_type, args: args } # validate :name, :presence, доппараметры
     end
+  end
 
   module InstanceValids
+
+    def validate! #  инстанс-метод validate!, который запускает все проверки (валидации) из validate
+      self.class.validations.each do |attrib, validations| 
+        attr_name = instance_variable_get("#{attrib}")
+        validations.each do |validation|
+          send( attr_name, validation[:validate_type], *validation[:args] ) #send(*args) public/ Invokes the method identified by symbol (attr), passing it any arguments specified.
+        end
+      end
+    end
 
     def valid_presence(attrib)
       raise "Attribute #{attrib} cannot be nil" if attrib.nil? 
@@ -34,17 +41,26 @@ module Validation
       raise "Attribute #{attrib} is not #{valid_format}" if value !~ valid_format
     end
 
-  def valid?
-    validate!
-  rescue StandardError
-    false
+    def valid?
+      validate!
+    rescue StandardError
+      false
+    end
   end
 
 end
 
-class Test
-  extend Validation
+class ValidTest
+  include Validation
 
-attr_accessor :per1, :per2
+attr_accessor :per1, :per2, :per3
 
-  end
+validate :per1, :presence
+end
+
+
+m = ValidTest.new
+m.per1 = 154
+m.per2 = '336_ps'
+m.per3 = nil
+
